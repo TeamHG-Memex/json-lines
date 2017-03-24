f6e2c59687674d664aeb49f5dd75b613ad087f6b
Mikhail Korobov <kmike84@gmail.com>
2017-03-24 15:26:36 +0500

    better recovery: handle multiple concatenated broken gzip archives

--- json_lines/_lib.py
+++ json_lines/_lib.py
@@ -1,9 +1,11 @@
+from __future__ import absolute_import
 from contextlib import contextmanager
-import gzip
 import json
+import gzip
 import logging
-
 import six
+
+from ._gzip import recover, get_known_read_position
 
 
 __all__ = ['open_file', 'reader', 'open']
@@ -34,21 +36,80 @@ def reader(file, broken=False):
     Read .jl or .jl.gz file with JSON lines data,
     return iterator with decoded lines.
     If the .jl.gz archive is broken as much lines as possible are read from
-    the archive, and then an error is logged.
+    the archive.
     """
-    try:
-        for line in file:
+    if not broken:
+        return _iter_json_lines(file)
+    else:
+        return _iter_json_lines_recovering(file)
+
+
+def _iter_json_lines(file):
+    return (_decode_json_line(line) for line in file)
+
+
+def _decode_json_line(line):
+    if not isinstance(line, six.text_type):
+        line = line.decode('utf8')
+    return json.loads(line)
+
+
+def _iter_json_lines_recovering(file):
+    is_gzip = hasattr(file, 'fileobj')
+    jl_reader = _JlRecoveringReader(file,
+                                    recover_gzip=is_gzip,
+                                    recover_jl=not is_gzip)
+    while True:
+        try:
+            for line in jl_reader.iter_lines():
+                yield line
+            return
+        except Exception as e:
+            logging.warning("Error found, trying to recover from %r" % e)
+            if not jl_reader.try_gzip_recovering():
+                logging.warning("Can't recover.")
+                return
+            logging.warning('Recovery successful, starting again from %s' %
+                            jl_reader.last_good_position)
+
+
+class _JlRecoveringReader(object):
+    def __init__(self, file, recover_gzip=True, recover_jl=False):
+        self.last_good_position = 0
+        self.file = file
+        self.recover_jl = recover_jl
+        self.recover_gzip = recover_gzip
+
+    def iter_lines(self):
+        for line in self.file:
             try:
-                if not isinstance(line, six.text_type):
-                    line = line.decode('utf8')
-                yield json.loads(line)
+                yield _decode_json_line(line)
             except Exception as e:
-                if not broken:
+                if not self.recover_jl:
                     raise
-                logging.warning(
-                    'Error found: JSON line can\'t be decoded. %r' % e)
-                break
-    except Exception as e:
-        if not broken:
-            raise
-        logging.warning('Error found: truncated archive. %r' % e)
+                logging.warning("Error: JSON line can't be decoded. %r" % e)
+            else:
+                if self.recover_gzip:
+                    self._mark_good(self.file, buffered=True)
+
+    def try_gzip_recovering(self):
+        if not self.recover_gzip:
+            return False
+
+        logging.debug(
+            "Position: %s current, %s certainly handled, %s last good" % (
+                self.file.fileobj.tell(),
+                get_known_read_position(self.file.fileobj),
+                self.last_good_position,
+            )
+        )
+        self.file = recover(self.file,
+                            last_good_position=self.last_good_position)
+        if self.file is None:
+            return False
+        self._mark_good(self.file, buffered=False)
+        return True
+
+    def _mark_good(self, file, buffered):
+        self.last_good_position = get_known_read_position(file.fileobj,
+                                                          buffered)
--- tests.py
+++ tests.py
@@ -1,18 +1,23 @@
 import gzip
 import json
+import random
 
 import pytest
 
 import json_lines
 
 
-def jl_bytes(data):
-    return '\n'.join(json.dumps(x) for x in data).encode('utf8')
+def write_gz(path, data, mode='wb'):
+    with gzip.open(str(path), mode) as f:
+        f.write(data)
 
 
 def write_jl_gz(path, data, mode='wb'):
-    with gzip.open(str(path), mode) as f:
-        f.write(jl_bytes(data))
+    write_gz(path, data=jl_bytes(data), mode=mode)
+
+
+def jl_bytes(data):
+    return '\n'.join(json.dumps(x) for x in data).encode('utf8')
 
 
 def test_reader_path(tmpdir):
@@ -53,7 +58,7 @@ def test_reader_gzip_path(tmpdir):
 
 def test_reader_broken_fail(tmpdir):
     p = tmpdir.join('myfile.jl.gz')
-    p.write('somedata')
+    p.write_binary(b'somedata')
 
     with json_lines.open_file(str(p)) as f_:
         with pytest.raises(Exception):
@@ -62,15 +67,14 @@ def test_reader_broken_fail(tmpdir):
 
 def test_reader_broken_json_fail(tmpdir):
     p = tmpdir.join('myfile.jl.gz')
-    with gzip.open(str(p), 'wb') as gzip_f:
-        gzip_f.write(b'{"a": 1}\n{[]')
+    write_gz(p, b'{"a": 1}\n{[]')
 
     with json_lines.open_file(str(p)) as f_:
         with pytest.raises(Exception):
             _ = list(json_lines.reader(f_))
 
 
-def test_reader_broken(tmpdir):
+def test_reader_broken_single(tmpdir):
     p = tmpdir.join('myfile.jl.gz')
     data = [{'a': 1}]
     write_jl_gz(p, data * 1000)
@@ -81,14 +85,47 @@ def test_reader_broken(tmpdir):
         assert read_data[:900] == data * 900
 
 
+def test_reader_broken_multiple(tmpdir):
+    p = tmpdir.join('myfile.jl.gz')
+    data_a = [{'a': random.random()} for _ in range(1000)]
+    data_b = [{'b': random.random()} for _ in range(1000)]
+
+    write_jl_gz(p, data_a)
+    p.write_binary(p.read_binary()[:-10])
+    write_jl_gz(p, data_b, 'ab')
+
+    with json_lines.open_file(str(p)) as f:
+        read_data = list(json_lines.reader(f, broken=True))
+        assert read_data[:900] == data_a[:900]  # first part is recovered
+        assert read_data[-1000:] == data_b  # second part is recovered
+
+
 def test_reader_broken_json(tmpdir):
     p = tmpdir.join('myfile.jl.gz')
-    with gzip.open(str(p), 'wb') as gzip_f:
-        gzip_f.write(b'{"a": 1}\n{[]')
+    write_gz(p, b'{"a": 1}\n{[]')
 
     with json_lines.open_file(str(p)) as f_:
         read_data = list(json_lines.reader(f_, broken=True))
         assert read_data == [{'a': 1}]
+
+
+def test_reader_broken_json_partial(tmpdir):
+    # with broken=True broken json lines are skipped, but reading continues
+    p = tmpdir.join('myfile.jl')
+    p.write_binary(b'{"a": 1}\n{"a": 2\n{"b": 1}\n')
+    with json_lines.open(str(p), broken=True) as f:
+        lines = list(f)
+    assert lines == [{'a': 1}, {'b': 1}]
+
+
+def test_reader_broken_json_partial_gzipped(tmpdir):
+    # For gzip files broken=True only means gzip recovery, inside a single
+    # archive processing stops at first broken json line
+    p = tmpdir.join('myfile.jl.gz')
+    write_gz(p, b'{"a": 1}\n{"a": 2\n{"b": 1}\n')
+    with json_lines.open(str(p), broken=True) as f:
+        lines = list(f)
+    assert lines == [{'a': 1}]
 
 
 def test_reader_broken_fuzz(tmpdir):
@@ -102,4 +139,3 @@ def test_reader_broken_fuzz(tmpdir):
         with json_lines.open_file(str(p)) as f_:
             read_data = list(json_lines.reader(f_, broken=True))
             assert isinstance(read_data, list)
-
