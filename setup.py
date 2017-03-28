a69092fb38c0230ba1729fdb3eadbef668f99de3
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2017-03-28 11:34:31 +0300

    Update release notes, bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='json-lines',
-    version='0.3.0',
+    version='0.3.1',
     description='Reading JSON lines (jl) files, recover broken files',
     license='MIT',
     url='https://github.com/TeamHG-Memex/json-lines',
